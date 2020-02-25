83b9e67e33c1abc7953a1cc1fd3397605c1ff223
valen22br <valensuela@gmail.com>
2020-02-25 15:26:58 -0500

    addressed edge cases

--- lengthEncodeDecode/lengthED.cpp
+++ lengthEncodeDecode/lengthED.cpp
@@ -6,41 +6,65 @@ using namespace std;
 
 string inputString;
 
-string run_length_encode(const string& inputString){
+string run_length_encode(const string &inputString)
+{
     string outputString = "";
     int count;
-    for( int i = 0; i < inputString.length(); i++)
+    for (int i = 0; i < inputString.length(); i++)
     {
         count = 1;
-        while(inputString[i] == inputString[i+1]){
+        while (inputString[i] == inputString[i + 1])
+        {
             count++;
             i++;
         }
-        outputString += inputString[i] + to_string(count);
+        if (count > 1)
+        {
+            outputString += inputString[i] + to_string(count);
+        }
+        else
+        {
+            outputString += inputString[i];
+        }
     }
-    
+
     return outputString;
 };
 
-string run_length_decode(const string& inputString){
+string run_length_decode(const string &inputString)
+{
     string outputString = "";
     int count, repetitions;
     char tempChar[10];
-    for( int i = 0; i < inputString.length(); i++){
-        if(isdigit(inputString[i])){
+    for (int i = 0; i < inputString.length(); i++)
+    {
+        if (isdigit(inputString[i]))
+        {
             inputString.copy(tempChar, 1, i);
             repetitions = atoi(tempChar);
-            for( int j = 1; j <= repetitions; j++){
-                outputString += inputString[i-1];
+            for (int j = 1; j < repetitions; j++)
+            {
+                outputString += inputString[i - 1];
             }
+        }
+        else
+        {
+            outputString += inputString[i];
         }
     }
     return outputString;
 }
 
-int main() {
-    inputString = "WWWBBWWWWW";
-    cout<<"run_length_encode "<<run_length_encode(inputString)<<endl;
-    cout<<"run_length_decode "<<run_length_decode(run_length_encode(inputString))<<endl;
+int main()
+{
+    cout << "Enter with a string to be coded. (ie. WWWBBWWWWW)";
+    cin >> inputString;
+    if (inputString.length() == 0)
+    {
+        inputString = "WWWBBWWWWW";
+    }
+
+    cout << "run_length_encode " << run_length_encode(inputString) << endl;
+    cout << "run_length_decode " << run_length_decode(run_length_encode(inputString)) << endl;
     return 0;
 }
